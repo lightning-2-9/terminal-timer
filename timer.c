f7ed6bf8430fa8fdbd5bb354640860df5ee4f744
lightning-2-9 <80739515+lightning-2-9@users.noreply.github.com>
2026-03-31 14:01:11 -0400

    [REDACTED]

--- timer.c
+++ timer.c
@@ -36,21 +36,21 @@ int run_error_check(int argc, char *argv[]) {
 	// Check for only 2|4 arguments
 	// Do this check before assigning seconds
 	if (argc != 2 && argc != 4) {
-		printf("You should be entering a single value!\n");
+		printf("What seconds?\n");
 		exit(1);
 	}
 	// Check if it's a number
 	int start = (argv[1][0] == '-') ? 1 : 0; // Check if first char is -; If yes then skip
     for (int i = start, n = strlen(argv[1]); i < n; i++) {
 		if (!isdigit(argv[1][i])) {
-			printf("You should be entering a number!\n");
+			printf("I don't understand.\n");
 			exit(2);
 		}
 	}
 	int seconds = atoi(argv[1]);
 	// Check for argument not being -ve
 	if (seconds < 0) {
-		printf("Number should be greater than 0... Negative time isn't possible yet!\n");
+		printf("Negative time isn't possible.\n\nYet...\n");
 		exit(3);
 	}
 	return seconds;
